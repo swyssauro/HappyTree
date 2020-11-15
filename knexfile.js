0347d1452e556be04248456cc61306e457e23d2d
sωıтcн <40813662+swyssauro@users.noreply.github.com>
2020-11-15 16:10:08 -0300

    ❤ update production_am

--- knexfile.js
+++ knexfile.js
@@ -29,19 +29,14 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'sqlite3',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      filename: './src/database/database.sqlite'
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './src/database/migrations',
+    },
+    useNullAsDefault: true,
   }
 
 };
